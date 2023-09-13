3512714a1693fba8a806b4d9fcb88a6572894dbf
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-13 20:27:40 +0300

    Create Domain layer

---
+++ src/Services/Identity/Identity.Domain/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Domain.Entities
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
