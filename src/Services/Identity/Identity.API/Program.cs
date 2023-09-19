00098481af36ea7896171cf514e8d06804d1345a
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-19 20:17:22 +0300

    Create Identity.API

--- src/Services/Identity/Identity.API/Program.cs
+++ src/Services/Identity/Identity.API/Program.cs
@@ -1,23 +1,32 @@
+using Identity.API.Configurations;
+using Identity.Application.Configurations;
+using Identity.Infrastructure.Configurations;
+using Identity.Infrastructure.DataContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.ConfigureIdentity(builder.Configuration);
+builder.Services.ConfigureServices();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGenConfiguration();
+
+builder.Services.AddAuthenticationConfiguration(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.ApplyMigrations();
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
