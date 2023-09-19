91ba7f1c4afea661ddf0549e5bba544ada9bce79
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-19 20:10:39 +0300

    Add exceptions

---
+++ src/Services/Identity/Identity.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,15 @@
+﻿namespace Identity.Application.Exceptions
+{
+    /// <summary>
+    /// Represents an exception that is thrown when a bad request is encountered.
+    /// </summary>
+    public class BadRequestException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the BadRequestException class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        public BadRequestException(string message) : base(message)
+        { }
+    }
+}
