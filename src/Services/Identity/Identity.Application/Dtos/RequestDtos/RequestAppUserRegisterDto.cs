3df1b6b7788d6c1f0696b464b259514c914dfc38
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-19 20:09:29 +0300

    Add requestDtos

---
+++ src/Services/Identity/Identity.Application/Dtos/RequestDtos/RequestAppUserRegisterDto.cs
@@ -0,0 +1,30 @@
+﻿using Identity.Domain.Enums;
+
+namespace Identity.Application.Dtos.RequestDtos
+{
+    /// <summary>
+    /// DTO for user registration request.
+    /// </summary>
+    public class RequestAppUserRegisterDto
+    {
+        /// <summary>
+        /// Gets or sets the username of the user.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date of birth of the user.
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the gender of the user.
+        /// </summary>
+        public Gender Gender { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password for the user's account.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
