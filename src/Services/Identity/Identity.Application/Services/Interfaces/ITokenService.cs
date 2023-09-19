a29fcbf39cbf149c6f1f7e65dce1d64fda2d649b
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-19 20:11:19 +0300

    Add interfaces for services

---
+++ src/Services/Identity/Identity.Application/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using Identity.Domain.Entities;
+
+namespace Identity.Application.Services.Interfaces
+{
+    /// <summary>
+    /// Provides methods for working with authentication tokens.
+    /// </summary>
+    public interface ITokenService
+    {
+        /// <summary>
+        /// Creates an authentication token for the specified user.
+        /// </summary>
+        /// <param name="user">The user for whom the token is created.</param>
+        /// <returns>The generated authentication token as a string.</returns>
+        Task<string> CreateTokenAsync(AppUser user);
+    }
+}
