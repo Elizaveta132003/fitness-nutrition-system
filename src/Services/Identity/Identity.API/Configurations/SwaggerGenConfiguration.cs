3499e32b08a650220be2b564a8665e7cb0efbf73
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-19 20:16:29 +0300

    Add swaggerGenConfiguration

---
+++ src/Services/Identity/Identity.API/Configurations/SwaggerGenConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace Identity.API.Configurations
+{
+    /// <summary>
+    /// Configuration class for adding Swagger generation settings.
+    /// </summary>
+    public static class SwaggerGenConfiguration
+    {
+        /// <summary>
+        /// Adds Swagger generation settings to the service collection.
+        /// </summary>
+        /// <param name="services">The collection of services to configure.</param>
+        public static void AddSwaggerGenConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+        }
+    }
+}
