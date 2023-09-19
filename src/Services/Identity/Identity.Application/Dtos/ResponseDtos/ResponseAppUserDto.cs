2eee1c811c9ffe2e2e5083fd097a391880e8af01
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-19 20:09:48 +0300

    Add responseDtos

---
+++ src/Services/Identity/Identity.Application/Dtos/ResponseDtos/ResponseAppUserDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Identity.Application.Dtos.ResponseDtos
+{
+    /// <summary>
+    /// DTO for user response.
+    /// </summary>
+    public class ResponseAppUserDto
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier of the user.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the username of the user.
+        /// </summary>
+        public string UserName { get; set; }
+    }
+}
