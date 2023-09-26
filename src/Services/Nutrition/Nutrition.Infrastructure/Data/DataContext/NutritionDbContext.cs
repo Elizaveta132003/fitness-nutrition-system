f62968de798861213c699d6a2596e09d44f74f20
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-26 11:47:08 +0300

    Add nutritionDbContext

---
+++ src/Services/Nutrition/Nutrition.Infrastructure/Data/DataContext/NutritionDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nutrition.Domain.Entities;
+using System.Reflection;
+
+namespace Nutrition.Infrastructure.Data.DataContext
+{
+    public class NutritionDbContext:DbContext
+    {
+        public virtual DbSet<Food> Foods { get; set; }
+        public virtual DbSet<FoodDiary> FoodDiarys { get;set; }
+        public virtual DbSet<MealDetail> MealDetails { get; set; }
+        public virtual DbSet<MealDish> MealDishes { get; set;}
+        public virtual DbSet<User> Users { get; set; }
+
+        public NutritionDbContext(DbContextOptions<NutritionDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
