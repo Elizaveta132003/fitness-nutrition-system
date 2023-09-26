fa142c48a0e97785c6f388931a421ac2fe91be75
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-26 09:39:14 +0300

    Add adminAttribute

---
+++ src/Services/Identity/Identity.API/Attributes/AdminAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Identity.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Identity.API.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public class AdminAttribute : AuthorizeAttribute
+    {
+        public AdminAttribute()
+        {
+            Roles = Role.admin.ToString();
+        }
+    }
+}
