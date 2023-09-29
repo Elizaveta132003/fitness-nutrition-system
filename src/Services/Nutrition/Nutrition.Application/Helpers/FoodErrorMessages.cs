4228f4099b08319a164b247e256a5c14edeb3cca
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-29 09:16:58 +0300

    Add helpers

---
+++ src/Services/Nutrition/Nutrition.Application/Helpers/FoodErrorMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Nutrition.Application.Helpers
+{
+    public static class FoodErrorMessages
+    {
+        public const string ProductAlreadyExists = "Product with that name already exists.";
+        public const string ProductNotFound = "Product not found.";
+    }
+}
+
---
+++ src/Services/Nutrition/Nutrition.Application/Helpers/UserErrorMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace Nutrition.Application.Helpers
+{
+    public class UserErrorMessages
+    {
+        public const string UserAlreadyExists = "User with that name already exists.";
+    }
+}
