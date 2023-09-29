1b0bd8236bb8fcb27354ef140c6b0bbce66f6397
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-29 09:18:16 +0300

    Add userCommands

---
+++ src/Services/Nutrition/Nutrition.Application/Features/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using Mapster;
+using MediatR;
+using Nutrition.Application.Dtos.ResponseDtos;
+using Nutrition.Application.Exceptions;
+using Nutrition.Application.Helpers;
+using Nutrition.Domain.Entities;
+using Nutrition.Domain.Interfaces.IRepositories;
+
+namespace Nutrition.Application.Features.Users.Commands.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserResponseDto>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IFoodDiaryRepository _foodDiaryRepository;
+
+        public CreateUserCommandHandler(IUserRepository userRepository, IFoodDiaryRepository foodDiaryRepository)
+        {
+            _userRepository = userRepository;
+            _foodDiaryRepository = foodDiaryRepository;
+        }
+
+        public async Task<UserResponseDto> Handle(CreateUserCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var isFoundUser = await _userRepository.UserExistsAsync(request.UserRequestDto.Username, 
+                cancellationToken);
+
+            if (isFoundUser)
+            {
+                throw new AlreadyExistsException(UserErrorMessages.UserAlreadyExists);
+            }
+
+            var user = request.UserRequestDto.Adapt<User>();
+            user.Id = Guid.NewGuid();
+
+            await _userRepository.CreateAsync(user, cancellationToken);
+
+            var foodDiary = new FoodDiary()
+            {
+                Id = Guid.NewGuid(),
+                UserId = user.Id
+            };
+
+            await _foodDiaryRepository.CreateAsync(foodDiary, cancellationToken);
+
+            await _foodDiaryRepository.SaveChangesAsync(cancellationToken); 
+
+            var userResponseDto=user.Adapt<UserResponseDto>();
+
+            return userResponseDto;
+            //TODO: Где будет обрабатываться что если юзер не создался или если не создалась foodDiary то будет откат всей транзакции
+        }
+    }
+}
---
+++ src/Services/Nutrition/Nutrition.Application/Features/Users/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Nutrition.Application.Validators.RequestValidators;
+
+namespace Nutrition.Application.Features.Users.Commands.CreateUser
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator() 
+        {
+            RuleFor(x => x.UserRequestDto)
+                .SetValidator(new UserRequestValidator());
+        }
+    }
+}
