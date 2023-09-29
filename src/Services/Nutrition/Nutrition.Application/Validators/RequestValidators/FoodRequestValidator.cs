cc8093b7b169b12c874bd21222322a446288247d
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-09-29 09:17:16 +0300

    Add validators

---
+++ src/Services/Nutrition/Nutrition.Application/Validators/RequestValidators/FoodRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Nutrition.Application.Dtos.RequestDtos;
+
+namespace Nutrition.Application.Validators.RequestValidators
+{
+    public class FoodRequestValidator: AbstractValidator<FoodRequestDto>
+    {
+        public FoodRequestValidator()
+        {
+            RuleFor(food => food.Name)
+                .NotEmpty()
+                .WithMessage("The food name cannot be empty.")
+                .NotNull()
+                .WithMessage("The food name cannot be null.");
+
+            RuleFor(food => food.Calories)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("Calories cannot be less than zero.");
+
+        }
+    }
+}
---
+++ src/Services/Nutrition/Nutrition.Application/Validators/RequestValidators/UserRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Nutrition.Application.Dtos.RequestDtos;
+
+namespace Nutrition.Application.Validators.RequestValidators
+{
+    public class UserRequestValidator:AbstractValidator<UserRequestDto>
+    {
+        public UserRequestValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Username is required.")
+                .MaximumLength(50)
+                .WithMessage("Username must not exceed 50 characters.");
+        }
+    }
+}
