d8a0cf9d5a961c8b706bc7d16750d94f2d8b1137
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-02 11:14:41 +0300

    Add helpers

--- src/Services/Nutrition/Nutrition.Application/Helpers/UserErrorMessages.cs
+++ src/Services/Nutrition/Nutrition.Application/Helpers/UserErrorMessages.cs
@@ -3,5 +3,6 @@
     public class UserErrorMessages
     {
         public const string UserAlreadyExists = "User with that name already exists.";
+        public const string UserNotFound = "User not found";
     }
 }
