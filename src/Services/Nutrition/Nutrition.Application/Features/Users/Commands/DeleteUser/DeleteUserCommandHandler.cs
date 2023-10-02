8a8d1d2dc6be53e74ab6bf735cff62596e0ef493
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-02 11:14:32 +0300

    Add user commands and queries

---
+++ src/Services/Nutrition/Nutrition.Application/Features/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Mapster;
+using MediatR;
+using Nutrition.Application.Dtos.ResponseDtos;
+using Nutrition.Application.Exceptions;
+using Nutrition.Application.Helpers;
+using Nutrition.Domain.Interfaces.IRepositories;
+
+namespace Nutrition.Application.Features.Users.Commands.DeleteUser
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, UserResponseDto>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public DeleteUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserResponseDto> Handle(DeleteUserCommand request,
+            CancellationToken cancellationToken)
+        {
+            var foundUser = await _userRepository.GetByIdAsync(request.id, cancellationToken);
+
+            if (foundUser is null)
+            {
+                throw new NotFoundException(UserErrorMessages.UserNotFound);
+            }
+
+            await _userRepository.DeleteAsync(foundUser, cancellationToken);
+
+            await _userRepository.SaveChangesAsync(cancellationToken);
+
+            var userResponseDto = foundUser.Adapt<UserResponseDto>();
+
+            return userResponseDto;
+        }
+    }
+}
---
+++ src/Services/Nutrition/Nutrition.Application/Features/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Mapster;
+using MediatR;
+using Nutrition.Application.Dtos.ResponseDtos;
+using Nutrition.Application.Exceptions;
+using Nutrition.Application.Helpers;
+using Nutrition.Domain.Entities;
+using Nutrition.Domain.Interfaces.IRepositories;
+
+namespace Nutrition.Application.Features.Users.Commands.UpdateUser
+{
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, UserResponseDto>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UpdateUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserResponseDto> Handle(UpdateUserCommand request,
+            CancellationToken cancellationToken)
+        {
+            var isFoundUser = await _userRepository.UserExistsAsync(
+                request.UserRequestDto.Username, cancellationToken);
+
+            if (!isFoundUser)
+            {
+                throw new NotFoundException(UserErrorMessages.UserNotFound);
+            }
+
+            var user = request.UserRequestDto.Adapt<User>();
+
+            await _userRepository.UpdateAsync(user, cancellationToken);
+
+            await _userRepository.SaveChangesAsync(cancellationToken);
+
+            var userResponseDto = user.Adapt<UserResponseDto>();
+
+            return userResponseDto;
+        }
+    }
+}
