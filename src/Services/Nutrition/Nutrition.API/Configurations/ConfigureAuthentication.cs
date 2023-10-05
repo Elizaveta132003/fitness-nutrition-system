7dba045b6750853aa021f64241f66a30f38bdaa3
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-05 10:18:19 +0300

    Add configurations

---
+++ src/Services/Nutrition/Nutrition.API/Configurations/ConfigureAuthentication.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Nutrition.API.Configurations
+{
+    public static class ConfigureAuthentication
+    {
+        public static void AddConfigureAuthentication(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication
+            (
+                auth =>
+                {
+                    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["JwtSettings:Issuer"],
+                        ValidAudience = configuration["JwtSettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(configuration.GetSection("JwtSettings:SecretKey").Value!))
+                    };
+                }
+            );
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                       .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                       .RequireAuthenticatedUser()
+                       .Build());
+            });
+        }
+    }
+}
---
+++ src/Services/Nutrition/Nutrition.API/Configurations/SwaggerGenConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Nutrition.API.Configurations
+{
+    public static class SwaggerGenConfiguration
+    {
+        public static void AddSwaggerGenConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(swaggerGenOptions =>
+            {
+                swaggerGenOptions.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+                });
+                swaggerGenOptions.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+                swaggerGenOptions.EnableAnnotations();
+            });
+        }
+    }
+}
