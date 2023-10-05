a9dc77a776b60eded05e0fb82f33fc45bc35ef37
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-05 10:17:57 +0300

    Add meadiatRExtensions

---
+++ src/Services/Nutrition/Nutrition.Application/Extensions/MediatRExtensions.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Nutrition.Application.Features.Food.Queries.GetAllFood;
+using Nutrition.Application.Middleware;
+
+namespace Nutrition.Application.Extensions
+{
+    public static class MediatRExtensions
+    {
+        public static void ConfigureMediatR(this IServiceCollection services)
+        {
+            services.AddMediatR(x => x.RegisterServicesFromAssembly(typeof(GetAllFoodQuery).Assembly))
+                .AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        }
+    }
+}
