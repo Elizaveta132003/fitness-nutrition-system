c15829302fd1243d860e1c54643fa1a43d4bdb3d
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-05 10:18:10 +0300

    Add attribute

---
+++ src/Services/Nutrition/Nutrition.API/Attributes/AdminAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Nutrition.Domain.Enums;
+
+namespace Nutrition.API.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public class AdminAttribute : AuthorizeAttribute
+    {
+        public AdminAttribute()
+        {
+            Roles = Role.admin.ToString();
+        }
+    }
+}
