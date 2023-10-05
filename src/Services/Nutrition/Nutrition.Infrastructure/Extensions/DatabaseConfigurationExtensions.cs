71dea3c98c2b8dec44f461004a56ae0c4dd80796
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-05 10:15:39 +0300

    Add databaseExtensions

---
+++ src/Services/Nutrition/Nutrition.Infrastructure/Extensions/DatabaseConfigurationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nutrition.Domain.Interfaces.IRepositories;
+using Nutrition.Infrastructure.Data.DataContext;
+using Nutrition.Infrastructure.Repositories;
+
+namespace Nutrition.Infrastructure.Extensions
+{
+    public static class DatabaseConfigurationExtensions
+    {
+        public static void ApplyMigrations(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<NutritionDbContext>(options =>
+            options.UseSqlServer(connectionString), ServiceLifetime.Scoped);
+
+            //services.AddScoped(IBaseRepository, BaseRepository);
+            services.AddScoped<IFoodDiaryRepository, FoodDiaryRepository>();
+            services.AddScoped<IFoodRepository, FoodRepository>();
+            services.AddScoped<IMealDetailRepository, MealDetailRepository>();
+            services.AddScoped<IMealDishRepository, MealDishRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
