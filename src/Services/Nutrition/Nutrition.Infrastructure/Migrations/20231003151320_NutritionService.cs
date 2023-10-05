76f9f40ada62adb81ddaa1bcdadc2088deb7d3f3
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-05 10:15:24 +0300

    Add migration

---
+++ src/Services/Nutrition/Nutrition.Infrastructure/Migrations/20231003151320_NutritionService.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nutrition.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NutritionService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Foods",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Calories = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Foods", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FoodDiarys",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodDiarys", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FoodDiarys_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MealDetails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FoodDiaryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MealType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MealDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MealDetails_FoodDiarys_FoodDiaryId",
+                        column: x => x.FoodDiaryId,
+                        principalTable: "FoodDiarys",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MealDishes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MealDetailId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FoodId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ServingSize = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MealDishes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MealDishes_Foods_FoodId",
+                        column: x => x.FoodId,
+                        principalTable: "Foods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MealDishes_MealDetails_MealDetailId",
+                        column: x => x.MealDetailId,
+                        principalTable: "MealDetails",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FoodDiarys_UserId",
+                table: "FoodDiarys",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MealDetails_FoodDiaryId",
+                table: "MealDetails",
+                column: "FoodDiaryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MealDishes_FoodId",
+                table: "MealDishes",
+                column: "FoodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MealDishes_MealDetailId",
+                table: "MealDishes",
+                column: "MealDetailId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MealDishes");
+
+            migrationBuilder.DropTable(
+                name: "Foods");
+
+            migrationBuilder.DropTable(
+                name: "MealDetails");
+
+            migrationBuilder.DropTable(
+                name: "FoodDiarys");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
