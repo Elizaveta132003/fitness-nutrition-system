fc32f10d2a05a66a9aa22c491d7b1ff503a31425
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-06 08:29:33 +0300

    Change IRepositories

--- src/Services/Nutrition/Nutrition.Domain/Interfaces/IRepositories/IFoodDiaryRepository.cs
+++ src/Services/Nutrition/Nutrition.Domain/Interfaces/IRepositories/IFoodDiaryRepository.cs
@@ -2,7 +2,8 @@
 
 namespace Nutrition.Domain.Interfaces.IRepositories
 {
-    public interface IFoodDiaryRepository:IBaseRepository<FoodDiary>
+    public interface IFoodDiaryRepository : IBaseRepository<FoodDiary>
     {
+        public Task<FoodDiary> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
     }
 }
--- src/Services/Nutrition/Nutrition.Domain/Interfaces/IRepositories/IMealDetailRepository.cs
+++ src/Services/Nutrition/Nutrition.Domain/Interfaces/IRepositories/IMealDetailRepository.cs
@@ -6,5 +6,6 @@ namespace Nutrition.Domain.Interfaces.IRepositories
     public interface IMealDetailRepository : IBaseRepository<MealDetail>
     {
         public Task<bool> MealDetailExistsAsync(DateTime date, MealType mealType, CancellationToken cancellationToken = default);
+        public Task<MealDetail> GetByDateAndMealTypeAsync(DateTime date, MealType type, CancellationToken cancellationToken = default);
     }
 }
