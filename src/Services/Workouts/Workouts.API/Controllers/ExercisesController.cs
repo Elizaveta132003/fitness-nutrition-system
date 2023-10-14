bc1a689286fa3d4bf164861aa83619f592955115
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-14 15:50:30 +0300

    Add controllers

---
+++ src/Services/Workouts/Workouts.API/Controllers/ExercisesController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Workouts.BusinessLogic.Dtos.RequestDtos;
+using Workouts.BusinessLogic.Dtos.ResponseDtos;
+using Workouts.BusinessLogic.Services.Interfaces;
+
+namespace Workouts.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExercisesController : ControllerBase
+    {
+        private readonly IExerciseService _exerciseService;
+
+        public ExercisesController(IExerciseService exerciseService)
+        {
+            _exerciseService = exerciseService;
+        }
+
+        //[Admin]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<ExerciseResponseDto>> CreateExerciseAsync([FromBody] ExerciseRequestDto exerciseRequestDto,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _exerciseService.CreateAsync(exerciseRequestDto);
+
+            return Ok(result);
+        }
+
+        //[Admin]
+        [HttpDelete("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<ExerciseResponseDto>> DeleteExerciseAsync(Guid id,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _exerciseService.DeleteAsync(id);
+
+            return Ok(result);
+        }
+
+        //[Admin]
+        [HttpPut("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<ExerciseResponseDto>> UpdateExerciseAsync(Guid id,
+            ExerciseRequestDto exerciseRequestDto,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _exerciseService.UpdateAsync(id, exerciseRequestDto);
+
+            return Ok(result);
+        }
+
+        //[Authorize]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<ExerciseResponseDto>>> GetAllExercisesAsync(CancellationToken cancellationToken = default)
+        {
+            var result = await _exerciseService.GetAllAsync();
+
+            return Ok(result);
+        }
+
+        //[Authorize]
+        [HttpGet("{name}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ExerciseResponseDto>> GetExerciseByName(string name, CancellationToken cancellationToken = default)
+        {
+            var result = await _exerciseService.GetByNameAsync(name);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/Services/Workouts/Workouts.API/Controllers/WorkoutExercisesController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Workouts.BusinessLogic.Dtos.RequestDtos;
+using Workouts.BusinessLogic.Dtos.ResponseDtos;
+using Workouts.BusinessLogic.Services.Interfaces;
+using Workouts.DataAccess.Enums;
+
+namespace Workouts.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkoutExercisesController : ControllerBase
+    {
+        private readonly IWorkoutExerciseService _workoutExerciseService;
+
+        public WorkoutExercisesController(IWorkoutExerciseService workoutExerciseService)
+        {
+            _workoutExerciseService = workoutExerciseService;
+        }
+
+        //[Authorize]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<WorkoutExerciseResponseDto>> CreateWorkoutExerciseAsync(
+            [FromBody] WorkoutExerciseRequestDto workoutExerciseRequestDto,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _workoutExerciseService.CreateAsync(workoutExerciseRequestDto);
+
+            return Ok(result);
+        }
+
+        //[Authorize]
+        [HttpDelete("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<WorkoutExerciseResponseDto>> DeleteWorkoutExerciseAsync(
+            Guid id, CancellationToken cancellationToken = default)
+        {
+            var result = await _workoutExerciseService.DeleteAsync(id);
+
+            return Ok(result);
+        }
+
+        //[Authorize]
+        [HttpGet("{userId:guid}/{date}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<WorkoutExerciseResponseDto>>> GetAllWorkoutExercisesByUserIdAndDateAsync(
+            Guid userId,
+            DateTime date,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _workoutExerciseService.GetAllWorkoutExercisesByUserIdAndDateAsync(
+                userId, date);
+
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpGet("{userId:guid}/{date}/{type}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<WorkoutExerciseResponseDto>>> GetAllWorkoutExercisesByUserIdAndDateAndTypeAsync(
+            Guid userId,
+            DateTime date,
+            ExerciseType type,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _workoutExerciseService.GetAllWorkoutExercisesByUserIdAndDateAndTypeAsync(
+                userId, date, type);
+
+            return Ok(result);
+        }
+    }
+}
