e9b1130209c36b89cb739b6f1fd08e87b13cf5a2
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-14 15:50:20 +0300

    Add configurations

---
+++ src/Services/Workouts/Workouts.API/Configurations/SwaggerGenConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace Workouts.API.Configurations
+{
+    public static class SwaggerGenConfiguration
+    {
+        public static void AddSwaggerGenConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+        }
+    }
+}
