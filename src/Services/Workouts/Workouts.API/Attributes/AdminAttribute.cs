ddc66ae65dcd53f176beacd20d462c8c48847df0
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-14 15:50:08 +0300

    Add attributes

---
+++ src/Services/Workouts/Workouts.API/Attributes/AdminAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Workouts.BusinessLogic.Enums;
+
+namespace Workouts.API.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public class AdminAttribute : AuthorizeAttribute
+    {
+        public AdminAttribute()
+        {
+            Roles = Role.admin.ToString();
+        }
+    }
+}
