fb0e855c6b7a527705a87f6776bb607a8c3fe1a9
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-14 15:48:01 +0300

    Add services

---
+++ src/Services/Workouts/Workouts.BusinessLogic/Services/Implementations/UserService.cs
@@ -0,0 +1,90 @@
+﻿using Mapster;
+using Workouts.BusinessLogic.Dtos.RequestDtos;
+using Workouts.BusinessLogic.Dtos.ResponseDtos;
+using Workouts.BusinessLogic.Exceptions;
+using Workouts.BusinessLogic.Helpers;
+using Workouts.BusinessLogic.Services.Interfaces;
+using Workouts.DataAccess.Entities;
+using Workouts.DataAccess.Repositories.Interfaces;
+
+namespace Workouts.BusinessLogic.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IExercisesDiaryRepository _exercisesDiaryRepository;
+
+        public UserService(IUserRepository userRepository, IExercisesDiaryRepository exercisesDiaryRepository)
+        {
+            _userRepository = userRepository;
+            _exercisesDiaryRepository = exercisesDiaryRepository;
+        }
+
+        public async Task<UserResponseDto> CreateAsync(UserRequestDto user, CancellationToken cancellationToken = default)
+        {
+            var foundUser = await _userRepository.GetOneByAsync(u => u.Username == user.Username, cancellationToken);
+
+            if (foundUser is not null)
+            {
+                throw new AlreadyExistsException(UserErrorMessages.UserAlreadyExists);
+            }
+
+            var createUser = user.Adapt<User>();
+            createUser.Id = Guid.NewGuid();
+
+            _userRepository.Create(createUser);
+
+            var exercisesDiary = new ExercisesDiary()
+            {
+                Id = Guid.NewGuid(),
+                UserId = createUser.Id
+            };
+
+            _exercisesDiaryRepository.Create(exercisesDiary);
+
+            await _exercisesDiaryRepository.SaveChangesAsync(cancellationToken);
+
+            var userResponseDto = createUser.Adapt<UserResponseDto>();
+
+            return userResponseDto;
+        }
+
+        public async Task<UserResponseDto> DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            var foundUser = await _userRepository.GetOneByAsync(user => user.Id == id, cancellationToken);
+
+            if (foundUser is null)
+            {
+                throw new NotFoundException(UserErrorMessages.UserNotFound);
+            }
+
+            _userRepository.Delete(foundUser);
+
+            await _userRepository.SaveChangesAsync(cancellationToken);
+
+            var userResponseDto = foundUser.Adapt<UserResponseDto>();
+
+            return userResponseDto;
+        }
+
+        public async Task<UserResponseDto> UpdateAsync(Guid id, UserRequestDto user, CancellationToken cancellationToken = default)
+        {
+            var foundUser = await _userRepository.GetOneByAsync(u => u.Username == user.Username, cancellationToken);
+
+            if (foundUser is null)
+            {
+                throw new NotFoundException(UserErrorMessages.UserNotFound);
+            }
+
+            var updateUser = user.Adapt<User>();
+
+            _userRepository.Update(updateUser);
+
+            await _userRepository.SaveChangesAsync(cancellationToken);
+
+            var userResponseDto = updateUser.Adapt<UserResponseDto>();
+
+            return userResponseDto;
+        }
+    }
+}
---
+++ src/Services/Workouts/Workouts.BusinessLogic/Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Workouts.BusinessLogic.Dtos.RequestDtos;
+using Workouts.BusinessLogic.Dtos.ResponseDtos;
+
+namespace Workouts.BusinessLogic.Services.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<UserResponseDto> CreateAsync(UserRequestDto user, CancellationToken cancellationToken = default);
+        public Task<UserResponseDto> UpdateAsync(Guid id, UserRequestDto user, CancellationToken cancellationToken = default);
+        public Task<UserResponseDto> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
