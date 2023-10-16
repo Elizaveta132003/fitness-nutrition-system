ef0146addf4ab4c16de209e150a90ec6556e9dbc
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-16 10:29:34 +0300

    Add extensions

---
+++ src/Services/Calories/Calories.DataAccess/Extensions/DatabaseConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Calories.DataAccess.Repositories.Implementations;
+using Calories.DataAccess.Repositories.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace Calories.DataAccess.Extensions
+{
+    public static class DatabaseConfigurationExtensions
+    {
+        public static void ConfigureMongo(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IMongoClient>(sp => new MongoClient(configuration["MongoSettings:Connection"]));
+            services.AddScoped<IMongoDatabase>(sp =>
+            {
+                var client = sp.GetRequiredService<IMongoClient>();
+                return client.GetDatabase(configuration.GetValue<string>("MongoSettings:DatabaseName"));
+            });
+
+            services.AddScoped<ICaloriesTrackingRepository, CaloriesTrackingRepository>();
+        }
+    }
+}
