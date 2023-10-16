c08e24c101a766023d29faeafee0c2651dadfdd7
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-16 10:29:02 +0300

    Add entities

---
+++ src/Services/Calories/Calories.DataAccess/Entities/CaloriesTracking.cs
@@ -0,0 +1,10 @@
+﻿namespace Calories.DataAccess.Entities
+{
+    public class CaloriesTracking : BaseEntity
+    {
+        public string UserId { get; set; }
+        public DateTime Date { get; set; }
+        public double CaloriesConsumed { get; set; } = 0;
+        public double CaloriesBurned { get; set; } = 0;
+    }
+}
