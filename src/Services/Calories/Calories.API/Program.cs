b21ab05ef710acb0b3b8f25d8c5e2fbe0dc67c6c
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-16 10:50:36 +0300

    Code refactoring

--- src/Services/Calories/Calories.API/Program.cs
+++ src/Services/Calories/Calories.API/Program.cs
@@ -1,4 +1,5 @@
 using Calories.API.Configurations;
+using Calories.API.Middleware;
 using Calories.BusinessLogic.Extensions;
 using Calories.DataAccess.Extensions;
 using FluentValidation.AspNetCore;
@@ -18,6 +19,7 @@ builder.Services.AddConfigureAuthentication(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
