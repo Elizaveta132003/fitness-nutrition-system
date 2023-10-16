ed6787902dfbf61aa373a94c66b815677adf0748
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-16 10:29:58 +0300

    Add dtos

---
+++ src/Services/Calories/Calories.BusinessLogic/Dtos/RequestDtos/CaloriesTrackingRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Calories.BusinessLogic.Dtos.RequestDtos
+{
+    public class CaloriesTrackingRequestDto
+    {
+        public string UserId { get; set; }
+        public DateTime Date { get; set; }
+        public double CaloriesConsumed { get; set; }
+        public double CaloriesBurned { get; set; }
+    }
+}
