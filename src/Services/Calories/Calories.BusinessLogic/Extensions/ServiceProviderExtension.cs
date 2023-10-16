558427e6b0e3b865ad4af2e86d2b139c582a3498
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-16 10:30:49 +0300

    Add extensions

---
+++ src/Services/Calories/Calories.BusinessLogic/Extensions/ServiceProviderExtension.cs
@@ -0,0 +1,14 @@
+﻿using Calories.BusinessLogic.Services.Implementations;
+using Calories.BusinessLogic.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Calories.BusinessLogic.Extensions
+{
+    public static class ServiceProviderExtension
+    {
+        public static void AddBusinessLogicService(this IServiceCollection services)
+        {
+            services.AddScoped<ICaloriesTrackingService, CaloriesTrackingService>();
+        }
+    }
+}
