ffd55938c9266d07d419ca6be64748c98f17c629
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-10-27 12:57:11 +0300

    Add nutritionKafkaHandler

---
+++ src/Services/Nutrition/Nutrition.Application/Events/NutritionKafkaHandler.cs
@@ -0,0 +1,47 @@
+﻿using Nutrition.Domain.Entities;
+using Nutrition.Domain.Interfaces.IRepositories;
+using Shared.Kafka.Consumer;
+using Shared.Kafka.Enums;
+using Shared.Kafka.Messages;
+
+namespace Nutrition.Application.Events
+{
+    public class NutritionKafkaHandler : IKafkaHandler<string, UserMessage>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public NutritionKafkaHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(string key, UserMessage value)
+        {
+            var user = new User()
+            {
+                Id = value.Id,
+                Username = value.Username
+            };
+
+            switch (value.MessageType)
+            {
+                case MessageType.Create:
+
+                    _userRepository.Create(user);
+
+                    await _userRepository.SaveChangesAsync();
+
+                    break;
+                case MessageType.Delete:
+
+                    _userRepository.Delete(user);
+
+                    await _userRepository.SaveChangesAsync();
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
