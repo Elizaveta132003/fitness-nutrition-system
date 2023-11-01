e7a9c646aa56687e42b5dba349265e83c1f57734
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-11-01 18:13:48 +0300

    Change repositories

--- src/Services/Calories/Calories.DataAccess/Repositories/Implementations/CaloriesTrackingRepository.cs
+++ src/Services/Calories/Calories.DataAccess/Repositories/Implementations/CaloriesTrackingRepository.cs
@@ -6,8 +6,27 @@ namespace Calories.DataAccess.Repositories.Implementations
 {
     public class CaloriesTrackingRepository : BaseRepository<CaloriesTracking>, ICaloriesTrackingRepository
     {
+        private readonly IMongoCollection<CaloriesTracking> _mongoCollection;
+
         public CaloriesTrackingRepository(IMongoDatabase database) : base(database)
         {
+            _mongoCollection = database.GetCollection<CaloriesTracking>(typeof(CaloriesTracking).Name);
+        }
+
+        public async Task<CaloriesTracking> GetByDateAndUserIdAsync(DateTime date, string userId,
+            CancellationToken cancellationToken = default)
+        {
+            var tomorrowDay = date.AddDays(1);
+
+            var filter = Builders<CaloriesTracking>.Filter.And(
+                Builders<CaloriesTracking>.Filter.Gte(x => x.Date, date),
+                Builders<CaloriesTracking>.Filter.Lt(x => x.Date, tomorrowDay),
+                Builders<CaloriesTracking>.Filter.Eq(x => x.UserId, userId));
+
+            var entity = await _mongoCollection.FindAsync(filter,
+                cancellationToken: cancellationToken);
+
+            return await entity.FirstOrDefaultAsync(cancellationToken);
         }
     }
 }
--- src/Services/Calories/Calories.DataAccess/Repositories/Interfaces/ICaloriesTrackingRepository.cs
+++ src/Services/Calories/Calories.DataAccess/Repositories/Interfaces/ICaloriesTrackingRepository.cs
@@ -4,5 +4,6 @@ namespace Calories.DataAccess.Repositories.Interfaces
 {
     public interface ICaloriesTrackingRepository : IBaseRepository<CaloriesTracking>
     {
+        Task<CaloriesTracking> GetByDateAndUserIdAsync(DateTime date, string userId, CancellationToken cancellationToken = default);
     }
 }
