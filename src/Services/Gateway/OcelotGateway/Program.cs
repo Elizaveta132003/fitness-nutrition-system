7ac10053826b1485978041e019027608599e1ff3
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-11-11 19:21:42 +0300

    Add Ocelot

---
+++ src/Services/Gateway/OcelotGateway/Program.cs
@@ -0,0 +1,26 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile(builder.Configuration["Ocelot:OcelotFile"]!, optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseSwaggerForOcelotUI(options =>
+    options.PathToSwaggerGenerator = "/swagger/docs");
+
+await app.UseOcelot();
+
+app.Run();
