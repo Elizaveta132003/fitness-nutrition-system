b76e9741a654e35b7bd9d9949ad0905b45a339be
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-11-11 11:14:07 +0300

    Add signalR

---
+++ src/Services/Nutrition/Nutrition.API/SignalR/MyHubHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Nutrition.API.Hubs;
+using Nutrition.Application.Interfaces.IBackgroundJobs;
+using Nutrition.Infrastructure.Interfaces;
+
+namespace Nutrition.API.SignalR
+{
+    public class MyHubHelper : IMyHubHelper
+    {
+        private readonly IHubContext<CaloriesHub> _hubContext;
+        private readonly IBackgroundJobsService _backgroundJobsService;
+
+        public MyHubHelper(IHubContext<CaloriesHub> hubContext, IBackgroundJobsService backgroundJobsService)
+        {
+            _hubContext = hubContext;
+            _backgroundJobsService = backgroundJobsService;
+        }
+
+        public async Task SendData(string userName, CancellationToken cancellationToken = default)
+        {
+            var currentDate = DateTime.Now;
+            var result = await _backgroundJobsService.CalculateCaloriesForUserAndDayAsync(userName, currentDate, cancellationToken);
+
+            await _hubContext.Clients.All.SendAsync("ReceiveCalories", result);
+        }
+    }
+}
