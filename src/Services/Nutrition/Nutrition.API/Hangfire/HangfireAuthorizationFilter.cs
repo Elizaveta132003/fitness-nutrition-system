3d14f80a61290dfb29b104d8adf19305429175c6
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-11-11 11:13:55 +0300

    Add hangfireAuthorizationeFilter

---
+++ src/Services/Nutrition/Nutrition.API/Hangfire/HangfireAuthorizationFilter.cs
@@ -0,0 +1,9 @@
+﻿using Hangfire.Dashboard;
+
+namespace Nutrition.API.Hangfire
+{
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context) => true;
+    }
+}
