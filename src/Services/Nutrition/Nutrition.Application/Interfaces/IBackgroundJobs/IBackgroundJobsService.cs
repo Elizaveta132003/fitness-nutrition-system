ff71e545d351359c30ddb58e0a1232559536dd83
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-11-11 11:11:55 +0300

    Add backgroundJobsService

---
+++ src/Services/Nutrition/Nutrition.Application/Interfaces/IBackgroundJobs/IBackgroundJobsService.cs
@@ -0,0 +1,8 @@
+﻿namespace Nutrition.Application.Interfaces.IBackgroundJobs
+{
+    public interface IBackgroundJobsService
+    {
+        Task<double> CalculateCaloriesForUserAndDayAsync(string userName, DateTime curreentDate,
+            CancellationToken cancellationToken = default);
+    }
+}
