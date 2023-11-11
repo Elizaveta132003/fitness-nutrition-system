7a9db5b5456130aca87a0c10a99448ccc807cefd
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-11-11 11:09:09 +0300

    Add client

--- client/src/http/index.tsx
+++ client/src/http/index.tsx
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-export const API_URL = "http://localhost:5198/api"
-
 const $api = axios.create({
     withCredentials: true,
-    baseURL: API_URL
 });
 
 $api.interceptors.request.use((config) => {
--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -5,10 +5,10 @@ import {IRegisterUser} from '../models/IRegisterUser';
 
 export default class AuthService {
     static login(userName: string, password: string): Promise<AxiosResponse<IAuthRepsponse>> {
-            return $api.post<IAuthRepsponse>('Users/authorization', {userName, password});
+            return $api.post<IAuthRepsponse>('http://localhost:5198/api/Users/authorization', {userName, password});
     }
 
     static async registration(user:IRegisterUser): Promise<AxiosResponse<IAuthRepsponse>> {
-        return $api.post<IAuthRepsponse>('Users/registration', user);
+        return $api.post<IAuthRepsponse>('http://localhost:5198/api/Users/registration', user);
     }
 }
