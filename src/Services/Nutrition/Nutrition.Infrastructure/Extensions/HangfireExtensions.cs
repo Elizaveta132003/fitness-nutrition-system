1c877f5ad6ff4a040f21d86917fb7958f4ad523c
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-11-11 11:13:08 +0300

    Add hangfireExtensions

---
+++ src/Services/Nutrition/Nutrition.Infrastructure/Extensions/HangfireExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Hangfire;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nutrition.Infrastructure.BackgroundTasks;
+
+namespace Nutrition.Infrastructure.Extensions
+{
+    public static class HangfireExtensions
+    {
+        public static void ConfigureHangfire(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("HangfireConnection");
+
+            services.AddHangfire(config => config
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(connectionString));
+
+            services.AddHangfireServer();
+
+            services.AddScoped<ICaloriesCalculationTask, CaloriesCalculationTask>();
+        }
+    }
+}
