5cf3d280bfb7c8574c54171380505cbc474224a2
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-11-13 09:55:27 +0300

    Add helper fo redis

---
+++ src/Services/Workouts/Workouts.BusinessLogic/Helpers/CacheHelper.cs
@@ -0,0 +1,9 @@
+﻿using Workouts.DataAccess.Entities;
+
+namespace Workouts.BusinessLogic.Helpers
+{
+    public static class CacheHelper
+    {
+        public static string GetCacheKeyForExercise(Guid id) => nameof(Exercise) + id;
+    }
+}
