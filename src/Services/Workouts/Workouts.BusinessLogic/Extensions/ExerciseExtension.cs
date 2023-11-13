7e77196d04ce02872e3f667582bce565ccc5c278
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-11-13 09:55:50 +0300

    Add extensions for redis

---
+++ src/Services/Workouts/Workouts.BusinessLogic/Extensions/ExerciseExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Options;
+using Workouts.BusinessLogic.Helpers;
+using Workouts.BusinessLogic.Options;
+using Workouts.DataAccess.Entities;
+
+namespace Workouts.BusinessLogic.Extensions
+{
+    public static class ExerciseExtension
+    {
+        public static async Task AddToCacheAsync(this Exercise exercise, IDistributedCache cache,
+            IOptions<RedisCacheOptions> options)
+        {
+            var cacheKey = CacheHelper.GetCacheKeyForExercise(exercise.Id);
+
+            var cacheOptions = new DistributedCacheEntryOptions()
+            {
+                SlidingExpiration = TimeSpan.FromMinutes(options.Value.SlidingExpirationTimeInMinutes)
+            };
+
+            await cache.SetAsync(cacheKey, exercise, cacheOptions);
+        }
+    }
+}
